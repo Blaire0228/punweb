53b67a028bb1177e3aba9bbf83097e437fc20195
Blaire0228 <yuki20040228@gmail.com>
2025-11-27 08:09:14 +0800

    初次上傳-空白後端框架

---
+++ src/main/java/pun/database/PunWeb/PunWebApplication.java
@@ -0,0 +1,13 @@
+package pun.database.PunWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunWebApplication.class, args);
+	}
+
+}
