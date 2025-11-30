d153d66d55b17ecf6c4bab39ac5c2578dd9c2a1d
Blaire0228 <yuki20040228@gmail.com>
2025-11-30 20:32:48 +0800

    嘗試寫User.java並連上資料庫

--- src/main/java/pun/database/PunWeb/model/User.java
+++ src/main/java/pun/database/PunWeb/model/User.java
@@ -1,4 +1,99 @@
 package pun.database.PunWeb.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String lastname;
+    private String firstname;
+    private String phone;
+    private String birthdate;
+    private String email;
+    private String password;
+    private String role;
+
+    //Constructor
+    public User(){
+        //JPA需要一個無參數建構子
+    }
+
+    public User(String lastname,String firstname,String phone,String birthdate,String email,String password,String role){
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.phone = phone;
+        this.birthdate = birthdate;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    //Getter & Setter
+    public Integer getId(){
+        return id;
+    }
+
+    public void setID(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastname(){
+        return lastname;
+    }
+
+    public void setLastname(String lastname){
+        this.lastname = lastname;
+    }
+
+    public String getFirstname(){
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    public String getBirthdate(){
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate){
+        this.birthdate = birthdate;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public void setRole(String role){
+        this.role = role;
+    }
 }
--- src/main/java/pun/database/PunWeb/repository/UserRepository.java
+++ src/main/java/pun/database/PunWeb/repository/UserRepository.java
@@ -1,4 +1,6 @@
 package pun.database.PunWeb.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import pun.database.PunWeb.model.User;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
